5245173916f3e27c805f07e192e89ccb4fc8c880
mpirazzi <pirazzi.marco@gmail.com>
2022-05-31 08:21:25 +0300

    readme fix

--- collision_detection_sat.py
+++ collision_detection_sat.py
@@ -11,11 +11,11 @@ m = 1
 #obj1_pos_series_x = [radius*math.sin(t*2*math.pi/steps_per_round) for t in range(steps)]
 #obj1_pos_series_y = [radius*math.cos(t*2*math.pi/steps_per_round) for t in range(steps)]
 
-obj1_pos_series_x = [m*t for t in range(steps)]
-obj1_pos_series_y = [50-m*t for t in range(steps)]
+obj1_pos_series_x = [50+m*t for t in range(steps)]
+obj1_pos_series_y = [150-m*t for t in range(steps)]
 
-obj2_pos_series_x = [-50+t for t in range(steps)]
-obj2_pos_series_y = [-50+m*t for t in range(steps)]
+obj2_pos_series_x = [50+t for t in range(steps)]
+obj2_pos_series_y = [50+m*t for t in range(steps)]
 
 object_1 = moving_object(0,0,0,type="Tractor",rel_x_start=0,rel_y_start=0,external_timestamp=0)
 object_2 = moving_object(0,0,1,type="Spraying drone",rel_x_start=45,rel_y_start=0,external_timestamp=0)
@@ -26,20 +26,23 @@ plt.axis('equal')
 plt.axis([-1000, 1000, -1000, 1000])
 ax.set_autoscale_on(False)
 
+zero_polygon = ([0,0,0,0],[0,0,0,0])
+
 plt.ion()
 p1 = plt.plot(0,0,".",color = "red")[0]
 p2 = plt.plot(0,0,".",color = "blue")[0]
-r1 = plt.plot(Polygon(object_1.vertices).exterior.xy,c = "red")[0]
-r2 = plt.plot(Polygon(object_2.vertices).exterior.xy,c = "blue")[0]
-r1_ss = plt.plot(Polygon(object_1.super_safe_vertices).exterior.xy,c = "red")[0]
-r2_ss = plt.plot(Polygon(object_2.super_safe_vertices).exterior.xy, c = "blue")[0]
+r1 = plt.plot(zero_polygon,c = "red")[0]
+r2 = plt.plot(zero_polygon,c = "blue")[0]
+r1_ss = plt.plot(zero_polygon,"--",c = "red")[0]
+r2_ss = plt.plot(zero_polygon,"--", c = "blue")[0]
 
 for i in range(steps):
     object_1.update_position_local(obj1_pos_series_x[i],obj1_pos_series_y[i],external_timestamp=i+1)
     object_2.update_position_local(obj2_pos_series_x[i],obj2_pos_series_y[i],external_timestamp=i+1)
     
-    ax.set_title(""+str(moving_separating_axis_theorem(object_1, object_2))+"\n"+ str(separating_axis_theorem(object_1, object_2)))
-    # plt.text(-50,-50,str(moving_separating_axis_theorem(object_1, object_2)))
-    # plt.text(50,50,str(separating_axis_theorem(object_1, object_2)))
+    res_str = "Collision time interval: "+str(moving_separating_axis_theorem(object_1, object_2))+"\n"\
+        + "Vehicle collision state: "+ str(separating_axis_theorem(object_1,object_2,super_safe=False))+"\n"\
+        + "Safe areas collision state: "+str(separating_axis_theorem(object_1, object_2))
+    ax.set_title(res_str)
     plot_object_lines(object_1,object_2,r1, r1_ss,r2, r2_ss,p1,p2)
     plt.pause(0.5)
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # **Collision detection and Avoidance tools**
-add a picture
+![](collision_resized.png)
 ## __Info__
 ca_utils is a python library meant for collision prediction, detection and (maybe) avoidance between both static and moving convex polygons
 
@@ -16,14 +16,15 @@ GPS coordinates received as Lat, Lon in WGS84 are translated into local cartesia
 
 ## Safety polygons
 Methods update_safety_rectangle and update_super_safe_rectangle of class moving_object are definig a rectangle oriented according to vehicle heading.  
-"Insert Image"
+![Safety Polygons](rectangles_resized.png)
 - Safe Polygon  
 Polygon representing the area occupied by the vehicle/object. Used for collision time computation
 - Super Safe Polygon  
 Used for collision detection only, representative of the safe area around the vehicle/object that shouldn't be crossed
 ## Static SAT
 Implementation of the separating axis theorem for 2D convex polygons  
-More at "add link"
+More at https://en.wikipedia.org/wiki/Hyperplane_separation_theorem
+![Separating axis theorem](SAT_resized.png)
 ## Moving SAT
 Separating axis theorem implementation based on the moving intervals time occupation of projection over axis. 
 The result is the interval __start of collision - end of collision__
@@ -32,7 +33,7 @@ The result is the interval __start of collision - end of collision__
 - [t0 > 0, t1 > 0]: Objects in collision course, collision starting in t0 seconds and ending in t1 seconds
 - [nan, nan]: Objects not in collision course
 - [t0 < 0, t1 > 0]: Objects already colliding, end of collision in t1 seconds
-- [t0 > 0, t1 < 0]: Objects are bending the space-time curve
+- [t0 < 0, t1 < 0]: Collision not happening / already happened
 
 ## Object Types Definition
 Objects name, safe & super-safe polygon relative coordinates and mission pause priority are defined in ca_utils.py.  
